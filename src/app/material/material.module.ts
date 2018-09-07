6491f524f912f86c42a05888f939aea1d7bf1a22
Brandon Ragsdale <brandorags@users.noreply.github.com>
2018-09-06 22:56:20 -0500

    created module exclusively for Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule,
+  MatIconModule, MatListModule } from '@angular/material';
+
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
