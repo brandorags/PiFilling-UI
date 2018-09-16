020a9fb9da18f65bff8f81240bee0e29a282babb
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-09-16 17:55:26 -0500

    added license notices and whitespace fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2018 Brandon Ragsdale
+ *
+ * This file is part of PiFilling-UI.
+ *
+ * PiFilling-UI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PiFilling-UI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PiFilling-UI.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 // Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
