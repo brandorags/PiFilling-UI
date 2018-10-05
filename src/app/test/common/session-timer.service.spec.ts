4ff6ad1637c6571693af96e88a2382d58cf0e149
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-10-04 23:05:37 -0500

    test case cleanup

--- src/app/test/common/session-timer.service.spec.ts
+++ src/app/test/common/session-timer.service.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { SessionTimerService } from './session-timer.service';
+import { SessionTimerService } from '../../common/session-timer.service';
 
 describe('SessionTimerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])
+      ],
       providers: [SessionTimerService]
     });
   });
