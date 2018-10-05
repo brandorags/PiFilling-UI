abd0561a4ff82adaf805c3fdce234e1a40ac8bb7
Brandon Ragsdale <brandorags@users.noreply.github.com>
2018-10-05 16:28:40 -0500

    added test cases for services

--- src/app/test/common/session-timer.service.spec.ts
+++ src/app/test/common/session-timer.service.spec.ts
@@ -1,21 +1,83 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { SimpleTimer } from 'ng2-simple-timer';
 
 import { SessionTimerService } from '../../common/session-timer.service';
 
 describe('SessionTimerService', () => {
+  let timer: SimpleTimer;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
       ],
-      providers: [SessionTimerService]
+      providers: [
+        SimpleTimer,
+        SessionTimerService
+      ]
     });
+
+    timer = TestBed.get(SimpleTimer);
   });
 
   it('should be created', inject([SessionTimerService], (service: SessionTimerService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should start timer', inject([SessionTimerService], (service: SessionTimerService) => {
+    let timerIds = timer.getTimer();
+    let subscriptionIds = timer.getSubscription();
+
+    expect(timerIds.length).toEqual(0);
+    expect(subscriptionIds.length).toEqual(0);
+
+    service.startTimer();
+
+    timerIds = timer.getTimer();
+    subscriptionIds = timer.getSubscription();
+
+    expect(timerIds.length).toEqual(1);
+    expect(subscriptionIds.length).toEqual(1);
+
+    let timerId = timerIds[0];
+    let subscriptionId = subscriptionIds[0];
+
+    expect(timerId).toEqual(service.sessionTimerId);
+    expect(subscriptionId).toContain(service.sessionTimerId);
+  }));
+
+  it('should stop timer', inject([SessionTimerService], (service: SessionTimerService) => {
+    service.startTimer();
+
+    let timerIds = timer.getTimer();
+    let subscriptionIds = timer.getSubscription();
+
+    expect(timerIds.length).toEqual(1);
+    expect(subscriptionIds.length).toEqual(1);
+
+    service.stopTimer();
+
+    timerIds = timer.getTimer();
+    subscriptionIds = timer.getSubscription();
+
+    expect(timerIds.length).toEqual(0);
+    expect(subscriptionIds.length).toEqual(0);
+  }));
+
+  it('should have a running timer', inject([SessionTimerService], (service: SessionTimerService) => {
+    service.startTimer();
+
+    expect(service.isTimerRunning()).toBeTruthy();
+  }));
+
+  it('should have a stopped timer', inject([SessionTimerService], (service: SessionTimerService) => {
+    service.startTimer();
+    service.stopTimer();
+
+    expect(service.isTimerRunning()).toBeFalsy();
   }));
 });
--- src/app/test/file/file.service.spec.ts
+++ src/app/test/file/file.service.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { FileService } from '../../file/file.service';
 
+import { Constants } from '../../common/constants';
+
 describe('FileService', () => {
+  let mockHttp: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [FileService]
     });
+
+    mockHttp = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    mockHttp.verify();
   });
 
   it('should be created', inject([FileService], (service: FileService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should upload the file', inject([FileService], (service: FileService) => {
+    let file = new FormData();
+
+    service.upload(file).subscribe();
+
+    let mockRequest = mockHttp.expectOne(Constants.apiBaseUrl + 'api/file/upload');
+
+    expect(mockRequest.request.method).toEqual('POST');
+    expect(mockRequest.request.withCredentials).toBeTruthy();
+    expect(mockRequest.request.reportProgress).toBeTruthy();
+    expect(mockRequest.request.responseType).toEqual('json');
   }));
 });
