fc785aec6a8d537c52a59ee3f6b80a92aa8ccec1
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-11-10 15:26:41 -0600

    added environment variable for file storage URL

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -2,6 +2,7 @@ import { environment } from '../../environments/environment';
 
 export class Constants {
   static readonly apiBaseUrl = environment.apiBaseUrl;
+  static readonly fileStorageBaseUrl = environment.fileStorageBaseUrl;
   static readonly lengthOfSession = environment.lengthOfSession;
   static readonly userCookieKey = 'currentUser';
   static readonly httpOptionsAuth = {
