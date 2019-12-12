a28cc482f9ec183aebe4b5c5794ec4ad61a57b0e
brandorags <brandorags@users.noreply.github.com>
2019-12-11 21:55:18 -0600

    functionality for downloading files

---
+++ src/app/file/file-action/download-helper.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+
+import { saveAs } from 'file-saver';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DownloadHelper {
+
+  saveFile(response: HttpResponse<any>): void {
+    let blob = new Blob([response.body]);
+    let contentDispositionHeader = response.headers.get('Content-Disposition');
+    let headerRegex = /filename=([^;]+)/ig.exec(contentDispositionHeader);
+    let filename = (headerRegex.length > 1) ? headerRegex[1].trim() : 'file';
+
+    saveAs(blob, filename);
+  }
+
+}
