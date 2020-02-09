74fbe44385ed3e4629ad3a155d78f63ac75133d4
brandorags <brandorags@users.noreply.github.com>
2020-02-08 23:54:00 -0600

    abstracted download functionality

--- src/app/file/file-action/download-helper.ts
+++ src/app/file/file-action/download-helper.ts
@@ -3,12 +3,59 @@ import { HttpResponse } from '@angular/common/http';
 
 import { saveAs } from 'file-saver';
 
+import { FileService } from '../file.service';
+import { SessionTimerService } from 'src/app/common/session-timer.service';
+
+import { FileDownload } from 'src/app/models/file/file-download';
+import { FileDelete } from 'src/app/models/file/file-delete';
+import { FileMetadata } from 'src/app/models/file/file-metadata';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadHelper {
 
-  saveFile(response: HttpResponse<any>): string {
+  constructor(
+    private fileService: FileService,
+    private sessionTimerService: SessionTimerService
+  ) { }
+
+  downloadFiles(files: FileMetadata[], folderPath: string): void {
+    let filesToDownload = [];
+    for (let f of files) {
+      if (!f.isSelected) {
+        continue;
+      }
+
+      filesToDownload.push(f);
+    }
+
+    let fileDownload = new FileDownload();
+    fileDownload.path = folderPath;
+    fileDownload.files = filesToDownload;
+
+    let filename: string;
+    this.fileService.downloadFiles(fileDownload)
+      .subscribe(response => {
+        if (response instanceof HttpResponse) {
+          filename = this.saveFile(response);
+        }
+
+        this.sessionTimerService.refreshTimer();
+      })
+      .add(() => {
+        if (filename && filename.split('.').pop() === 'zip') {
+          let fileToDelete = new FileDelete();
+          fileToDelete.filename = filename;
+          fileToDelete.path = '';
+          fileToDelete.isDirectory = false;
+
+          this.fileService.deleteFiles([fileToDelete]).subscribe();
+        }
+      });
+  }
+
+  private saveFile(response: HttpResponse<any>): string {
     let blob = new Blob([response.body]);
     let contentDispositionHeader = response.headers.get('Content-Disposition');
     let headerRegex = /filename=([^;]+)/ig.exec(contentDispositionHeader);
